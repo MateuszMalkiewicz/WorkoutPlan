725315a993e6f3474fd48937ba05231d492d520c
Mateusz Małkiewicz <mathew.malkiewicz@gmail.com>
2020-01-16 16:58:38 +0100

    UI improvement for listing of exercises, splits and workouts

---
+++ static/app.js
@@ -0,0 +1,16 @@
+$(function(){
+    let addExerciseButton = $("#add-exercise-button");
+    addExerciseButton.click(function () {
+        location.href = "/add_exercise/";
+    });
+
+    let addSplitButton = $("#add-split-button");
+    addSplitButton.click(function () {
+        location.href = "/add_split/";
+    });
+
+    let addWorkoutButton = $("#add-workout-button");
+    addWorkoutButton.click(function () {
+        location.href = "/add_workout/";
+    });
+});
