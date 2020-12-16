a3b5698994f562abf9c4a7b83b530cb8d4107f74
Mateusz Małkiewicz <41550579+MateuszMalkiewicz@users.noreply.github.com>
2020-12-16 20:06:48 +0100

    Delete local_settings.py

--- WorkoutPlanner/local_settings.py
+++
@@ -1,9 +0,0 @@
-SETTINGS = {
-    'default': {
-        'HOST': 'localhost',
-        'NAME': 'workout',
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
-        'PASSWORD': 'workout123',
-    }
-}
